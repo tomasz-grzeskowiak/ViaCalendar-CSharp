ae4eb2edcdbbe4533d53aca57d2f436e6d6627ce
Snopeczek <bsnopeczek@interia.pl>
2025-10-26 19:34:26 +0100

    Blazer App implementation

---
+++ ViaCalendarApp/Program.cs
@@ -0,0 +1,45 @@
+using ViaCalendarApp.Components;
+using ViaCalendarApp.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services
+builder.Services.AddRazorPages();             
+builder.Services.AddServerSideBlazor();       
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var httpClientBuilder = builder.Services.AddHttpClient<EventServiceClient>(client =>
+{
+
+    client.BaseAddress = new Uri("http://localhost:5096");
+    client.Timeout = TimeSpan.FromSeconds(10);
+});
+if (builder.Environment.IsDevelopment())
+{
+    httpClientBuilder.ConfigurePrimaryHttpMessageHandler(() =>
+        new HttpClientHandler
+        {
+            ServerCertificateCustomValidationCallback =
+                HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+        });
+}
+
+var app = builder.Build();
+
+// Middleware
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+app.Run();
---
+++ ViaCalendarApp/Services/EventServiceClient.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using APIContracts.DTOs.Event;
+
+namespace ViaCalendarApp.Services;
+
+public class EventServiceClient
+{
+    private readonly HttpClient _http;
+
+    public EventServiceClient(HttpClient http)
+    {
+        _http = http;
+    }
+
+    // Get all companies
+    public async Task<List<EventDto>> GetAllAsync()
+    {
+        return await _http.GetFromJsonAsync<List<EventDto>>("event") ?? new List<EventDto>();
+    }
+
+    // Create a event
+    public async Task CreateAsync(CreateEventDto eventDto)
+    {
+        var response = await _http.PostAsJsonAsync("event", eventDto);
+        response.EnsureSuccessStatusCode();
+    }
+
+    // Update a event
+    public async Task UpdateAsync(CreateEventDto eventDto)
+    {
+        var response = await _http.PutAsJsonAsync("event", eventDto);
+        response.EnsureSuccessStatusCode();
+    }
+   
+}
